34b6fd0348eec8cb17c320fc2b26189e436f830d
Goi Anastasiia <None>
2024-10-25 10:24:47 +0300

    HW2 is completed

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/fixme_my_friend/hw02_fix_app/printer"
-	"github.com/fixme_my_friend/hw02_fix_app/reader"
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"github.com/overgoy/home_work_/hw02_fix_app/printer"
+	"github.com/overgoy/home_work_/hw02_fix_app/reader"
+	"github.com/overgoy/home_work_/hw02_fix_app/types"
 )
 
 func main() {
