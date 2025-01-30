4069be8bbb4359aa8eec26bbb90abc0fb845df74
Goi Anastasiia <None>
2025-01-30 16:14:46 +0300

    hw07 is coplited

--- hw07_word_counter/main.go
+++ hw07_word_counter/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func countWords(text string) map[string]int {
+	wordCount := make(map[string]int)
+
+	text = strings.ToLower(text)
+
+	re := regexp.MustCompile(`[^\wа-яА-ЯёЁ]+`)
+	text = re.ReplaceAllString(text, " ")
+
+	words := strings.Fields(text)
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	return wordCount
+}
+
 func main() {
-	// Place your code here.
+	fmt.Println("Введите текст:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan() // Читаем строку с ввода
+
+	text := scanner.Text()
+	result := countWords(text)
+
+	fmt.Println("Частота слов:", result)
 }
