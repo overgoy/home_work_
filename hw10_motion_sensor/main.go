2ea5f8889bf48d0e450f579ce59dfddec3cb0468
Goi Anastasiia <None>
2025-02-02 12:18:17 +0300

    hw10 is complited

--- hw10_motion_sensor/main.go
+++ hw10_motion_sensor/main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func sensorDataGenerator(dataChan chan<- float64) {
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	timeout := time.After(60 * time.Second)
+
+	for {
+		select {
+		case <-timeout:
+			close(dataChan)
+			return
+		case <-ticker.C:
+			data := rand.Float64() * 100
+			dataChan <- data
+		}
+	}
+}
+
+func dataProcessor(dataChan <-chan float64, resultChan chan<- float64) {
+	var buffer []float64
+
+	for value := range dataChan {
+		buffer = append(buffer, value)
+
+		if len(buffer) == 10 {
+			avg := calculateAverage(buffer)
+			resultChan <- avg
+			buffer = nil
+		}
+	}
+
+	close(resultChan)
+}
+
+func calculateAverage(values []float64) float64 {
+	if len(values) == 0 {
+		return 0
+	}
+
+	sum := 0.0
+	for _, v := range values {
+		sum += v
+	}
+	return sum / float64(len(values))
+}
+
 func main() {
-	// Place your code here.
+	dataChan := make(chan float64)
+	resultChan := make(chan float64)
+
+	go sensorDataGenerator(dataChan)
+	go dataProcessor(dataChan, resultChan)
+
+	for avg := range resultChan {
+		fmt.Printf("Среднее значение: %.2f\n", avg)
+	}
 }
