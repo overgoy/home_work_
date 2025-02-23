7dc049fd30641cd95e21e96a48acd961381687c7
Goi Anastasiia <None>
2025-02-23 15:25:01 +0300

    hw16 is complited

--- hw16_docker/cmd/main.go
+++ hw16_docker/cmd/main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/fixme_my_friend/hw16_docker/db"
 	"github.com/fixme_my_friend/hw16_docker/internal"
-
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	dbConn, err := db.Connect()
 	if err != nil {
-		log.Fatal("Ошибка подключения к БД:", err)
+		log.Fatal(fmt.Errorf("Ошибка подключения к БД: %w", err))
 	}
 	defer dbConn.Close()
 
 	err = db.CreateTablesIfNotExist(dbConn)
 	if err != nil {
-		log.Fatal("Ошибка при создании таблиц:", err)
+		log.Fatal(fmt.Errorf("Ошибка при создании таблиц: %w", err))
 	}
 
 	internal.StartServer(dbConn)
